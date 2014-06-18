c7c2e4f33f577d9e602eac1b04e9a5cbb029af64
Bradley Ryan <bradryan13@gmail.com>
2014-06-18 11:15:59 -0600

    Header and base files

---
+++ views/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+  <header>
+  	
+  	<div class="row">
+  		<div>
+  			HELLO
+  		</div>
+
+  		<nav>
+
+  		</nav>
+  	</div>
+
+  </header>
